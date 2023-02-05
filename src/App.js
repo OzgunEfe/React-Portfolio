d71caf8afa66a58de4ccbcd507ba6be5f611da26
OzgunEfe <ozgunefe.dev@gmail.com>
2023-02-05 18:54:21 +0000

    I finished Homepage

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import NavBar from './components/nav'
+import Main from './components/main'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <NavBar />
+      </nav>
+      <main>
+        <Main />
+      </main>
+        
+
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <a href="#">About</a>
+            <a href="#">Work</a>
+            <a href="#">Contact</a>
+            <button>Resume</button>
+        </div>
+    )
+}
+
+export default NavBar;
