a585d8ecc02cc22f897d0aa08def82e6590cb709
OzgunEfe <56911896+OzgunEfe@users.noreply.github.com>
2023-03-05 11:53:47 +0000

    Update Header.js
    Change CV link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 function Header() {
     
     const handleDownload = () => {
-      window.open("https://drive.google.com/file/d/1YeNKGB5F6uNwKWP2i-C9fJ29OmQSsmXy/view?usp=sharing", "_blank");
+      window.open("https://drive.google.com/file/d/10U95X6jfvmqfrV6EQbc8aORQ7JoMXfc1/view", "_blank");
     };
   
     return (
